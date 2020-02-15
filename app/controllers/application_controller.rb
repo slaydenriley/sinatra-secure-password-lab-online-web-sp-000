829981650bf7c39be8a4c63df1c2a2757105a178
Riley Slayden <slaydenriley@gmail.com>
2020-02-15 18:33:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < Sinatra::Base
     if params[:username] == "" && params[:password] == ""
       redirect '/failure'
     else
+      User.create(username: params[:username], password: params[:password])
       redirect '/login'
     end
 
